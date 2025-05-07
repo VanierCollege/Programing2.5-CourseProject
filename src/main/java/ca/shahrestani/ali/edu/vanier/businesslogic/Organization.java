d56233d1793dbc3723f01acccc487fdefbca0c9e
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2025-05-06 21:53:01 -0400

    Implement basic test for Organization  class

--- src/main/java/ca/shahrestani/ali/edu/vanier/businesslogic/Organization.java
+++ src/main/java/ca/shahrestani/ali/edu/vanier/businesslogic/Organization.java
@@ -3,25 +3,93 @@ package ca.shahrestani.ali.edu.vanier.businesslogic;
 import ca.shahrestani.ali.edu.vanier.tool.Savable;
 import ca.shahrestani.ali.edu.vanier.tool.SavableFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Objects;
 
 public class Organization implements Savable {
     private String name;
-    private List<User> users;
-    private List<Project> projects;
-    private List<Account> accounts;
+    private Set<User> users;
+    private Set<Project> projects;
+    private Set<Account> accounts;
 
     public Organization(String name) {
         this(name, null, null, null);
     }
 
-    public Organization(String name, List<User> users, List<Project> projects, List<Account> accounts) {
+    public Organization(String name, Set<User> users, Set<Project> projects, Set<Account> accounts) {
         this.name = name;
-        this.users = Objects.requireNonNullElse(users, new ArrayList<>());
-        this.projects = Objects.requireNonNullElse(projects, new ArrayList<>());
-        this.accounts = Objects.requireNonNullElse(accounts, new ArrayList<>());
+        this.users = Objects.requireNonNullElse(users, new HashSet<>());
+        this.projects = Objects.requireNonNullElse(projects, new HashSet<>());
+        this.accounts = Objects.requireNonNullElse(accounts, new HashSet<>());
+    }
+
+    /**
+     * Add a user to the organization's list
+     *
+     * @param user the user to add
+     * @return whether the user is new (already added) or not
+     */
+    public boolean addUser(User user) {
+        Objects.requireNonNull(user);
+        return users.add(user);
+    }
+
+    /**
+     * Remove a user from the organization's list
+     *
+     * @param user the user to add
+     * @return whether the user was present or not
+     */
+    public boolean removeUser(User user) {
+        Objects.requireNonNull(user);
+        return users.remove(user);
+    }
+
+    /**
+     * Add a project to the organization's list
+     *
+     * @param project the project to add
+     * @return whether the project is new (already added) or not
+     */
+    public boolean addProject(Project project) {
+        Objects.requireNonNull(project);
+        return projects.add(project);
+    }
+
+    /**
+     * Remove a project from the organization's list
+     *
+     * @param project the project to add
+     * @return whether the project was present or not
+     */
+    public boolean removeProject(Project project) {
+        Objects.requireNonNull(project);
+        return projects.remove(project);
+    }
+
+
+
+    /**
+     * Add an account to the organization's list
+     *
+     * @param account the account to add
+     * @return whether the account is new (already added) or not
+     */
+    public boolean addAccount(Account account) {
+        Objects.requireNonNull(account);
+        return accounts.add(account);
+    }
+
+    /**
+     * Remove an account from the organization's list
+     *
+     * @param account the account to add
+     * @return whether the account was present or not
+     */
+    public boolean removeAccount(Account account) {
+        Objects.requireNonNull(account);
+        return accounts.remove(account);
     }
 
     /* OVERRIDE METHODS */
@@ -42,15 +110,15 @@ public class Organization implements Savable {
         return name;
     }
 
-    public List<User> getUsers() {
+    public Set<User> getUsers() {
         return users;
     }
 
-    public List<Project> getProjects() {
+    public Set<Project> getProjects() {
         return projects;
     }
 
-    public List<Account> getAccounts() {
+    public Set<Account> getAccounts() {
         return accounts;
     }
 
---
+++ src/test/java/businesslogic/OrganizationTest.java
@@ -0,0 +1,135 @@
+package businesslogic;
+
+import ca.shahrestani.ali.edu.vanier.businesslogic.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrganizationTest {
+
+    @Test
+    public void testAddUser_new() {
+        Organization organization = new Organization("Test");
+        User user = SystemAdministrator.newDefaultSystemAdministrator("Test");
+        Assertions.assertTrue(organization.addUser(user));
+    }
+
+    @Test
+    public void testAddUser_duplicate() {
+        Organization organization = new Organization("Test");
+        User user = SystemAdministrator.newDefaultSystemAdministrator("Test");
+        organization.addUser(user);
+        Assertions.assertFalse(organization.addUser(user));
+    }
+
+    @Test
+    public void testAddUser_null() {
+        Organization organization = new Organization("Test");
+        Assertions.assertThrows(NullPointerException.class, () -> organization.addUser(null));
+    }
+
+    @Test
+    public void testRemoveUser_true() {
+        Organization organization = new Organization("Test");
+        User user = SystemAdministrator.newDefaultSystemAdministrator("Test");
+        organization.addUser(user);
+        Assertions.assertTrue(organization.removeUser(user));
+    }
+
+    @Test
+    public void testRemoveUser_false() {
+        Organization organization = new Organization("Test");
+        User user = SystemAdministrator.newDefaultSystemAdministrator("Test");
+        Assertions.assertFalse(organization.removeUser(user));
+    }
+
+    @Test
+    public void testRemoveUser_null() {
+        Organization organization = new Organization("Test");
+        Assertions.assertThrows(NullPointerException.class, () -> organization.removeUser(null));
+    }
+
+    @Test
+    public void testAddProject_new() {
+        Organization organization = new Organization("Test");
+        Project project = new Project("Test", "Test", null);
+        Assertions.assertTrue(organization.addProject(project));
+    }
+
+    @Test
+    public void testAddProject_duplicate() {
+        Organization organization = new Organization("Test");
+        Project project = new Project("Test", "Test", null);
+        organization.addProject(project);
+        Assertions.assertFalse(organization.addProject(project));
+    }
+
+    @Test
+    public void testAddProject_null() {
+        Organization organization = new Organization("Test");
+        Assertions.assertThrows(NullPointerException.class, () -> organization.addProject(null));
+    }
+
+    @Test
+    public void testRemoveProject_true() {
+        Organization organization = new Organization("Test");
+        Project project = new Project("Test", "Test", null);
+        organization.addProject(project);
+        Assertions.assertTrue(organization.removeProject(project));
+    }
+
+    @Test
+    public void testRemoveProject_false() {
+        Organization organization = new Organization("Test");
+        Project project = new Project("Test", "Test", null);
+        Assertions.assertFalse(organization.removeProject(project));
+    }
+
+    @Test
+    public void testRemoveProject_null() {
+        Organization organization = new Organization("Test");
+        Assertions.assertThrows(NullPointerException.class, () -> organization.removeProject(null));
+    }
+
+    @Test
+    public void testAddAccount_new() {
+        Organization organization = new Organization("Test");
+        Account account = new PersonalAccount("Test");
+        Assertions.assertTrue(organization.addAccount(account));
+    }
+
+    @Test
+    public void testAddAccount_duplicate() {
+        Organization organization = new Organization("Test");
+        Account account = new PersonalAccount("Test");
+        organization.addAccount(account);
+        Assertions.assertFalse(organization.addAccount(account));
+    }
+
+    @Test
+    public void testAddAccount_null() {
+        Organization organization = new Organization("Test");
+        Assertions.assertThrows(NullPointerException.class, () -> organization.addAccount(null));
+    }
+
+    @Test
+    public void testRemoveAccount_true() {
+        Organization organization = new Organization("Test");
+        Account account = new PersonalAccount("Test");
+        organization.addAccount(account);
+        Assertions.assertTrue(organization.removeAccount(account));
+    }
+
+    @Test
+    public void testRemoveAccount_false() {
+        Organization organization = new Organization("Test");
+        Account account = new PersonalAccount("Test");
+        Assertions.assertFalse(organization.removeAccount(account));
+    }
+
+    @Test
+    public void testRemoveAccount_null() {
+        Organization organization = new Organization("Test");
+        Assertions.assertThrows(NullPointerException.class, () -> organization.removeAccount(null));
+    }
+
+}
