42e58671f65cee4632a55f7bc8fbeaacbf3dffb9
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2025-05-06 21:26:55 -0400

    Implement Organization class fields

--- src/main/java/ca/shahrestani/ali/edu/vanier/businesslogic/Organization.java
+++ src/main/java/ca/shahrestani/ali/edu/vanier/businesslogic/Organization.java
@@ -3,8 +3,56 @@ package ca.shahrestani.ali.edu.vanier.businesslogic;
 import ca.shahrestani.ali.edu.vanier.tool.Savable;
 import ca.shahrestani.ali.edu.vanier.tool.SavableFactory;
 
-// TODO
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Organization implements Savable {
+    private String name;
+    private List<User> users;
+    private List<Project> projects;
+    private List<Account> accounts;
+
+    public Organization(String name) {
+        this(name, null, null, null);
+    }
+
+    public Organization(String name, List<User> users, List<Project> projects, List<Account> accounts) {
+        this.name = name;
+        this.users = Objects.requireNonNullElse(users, new ArrayList<>());
+        this.projects = Objects.requireNonNullElse(projects, new ArrayList<>());
+        this.accounts = Objects.requireNonNullElse(accounts, new ArrayList<>());
+    }
+
+    /* OVERRIDE METHODS */
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "name='" + name + '\'' +
+                ", users=" + users +
+                ", projects=" + projects +
+                ", accounts=" + accounts +
+                '}';
+    }
+
+    /* GETTERS & SETTERS */
+
+    public String getName() {
+        return name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
 
     /* SAVABLE METHODS */
 
